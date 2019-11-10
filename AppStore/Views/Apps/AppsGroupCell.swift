3506cf348bb5585f56e4794928b7612c6bca06b5
Mateus Rovari <mateusrovari@Mateuss-MacBook-Pro.local>
2019-11-10 14:01:54 -0300

    Nested UICollectionViews

---
+++ AppStore/Views/Apps/AppsGroupCell.swift
@@ -0,0 +1,42 @@
+//
+//  AppsGroupCell.swift
+//  AppStore
+//
+//  Created by Mateus Rovari on 10/11/19.
+//  Copyright © 2019 Mateus Rovari. All rights reserved.
+//
+
+import UIKit
+
+extension UILabel {
+    convenience init(text: String, font: UIFont) {
+        self.init(frame: .zero)
+        self.text = text
+        self.font = font
+    }
+}
+
+class AppsGroupCell: UICollectionViewCell {
+    
+    let titleLabel = UILabel(text: "App Section", font: .boldSystemFont(ofSize: 30))
+    
+    let horizontalController = AppsHorizontalController()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        backgroundColor = .purple
+        
+        addSubview(titleLabel)
+        titleLabel.anchor(top: topAnchor, leading: leadingAnchor, bottom: nil, trailing: trailingAnchor)
+        
+        addSubview(horizontalController.view)
+        horizontalController.view.backgroundColor = .blue
+        horizontalController.view.anchor(top: titleLabel.bottomAnchor, leading: leadingAnchor, bottom: bottomAnchor, trailing: trailingAnchor)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
