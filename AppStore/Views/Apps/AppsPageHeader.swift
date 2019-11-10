ab3b92a5071c9801c95271f1fadce7714b9010c4
Mateus Rovari <mateusrovari@Mateuss-MacBook-Pro.local>
2019-11-10 20:16:31 -0300

    Nested Header Collection View

---
+++ AppStore/Views/Apps/AppsPageHeader.swift
@@ -0,0 +1,25 @@
+//
+//  AppsPageHeader.swift
+//  AppStore
+//
+//  Created by Mateus Rovari on 10/11/19.
+//  Copyright © 2019 Mateus Rovari. All rights reserved.
+//
+
+import UIKit
+
+class AppsPageHeader: UICollectionReusableView {
+    
+    let appHeaderHorizontalController = AppsHeaderHorizontalController()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        addSubview(appHeaderHorizontalController.view)
+        appHeaderHorizontalController.view.fillSuperview()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
