3b56ccaf71ced4c8a1ecc8e3ba3e6069f4fd55ee
Mateus Rovari <mateusrovari@Mateuss-MacBook-Pro.local>
2019-11-13 23:45:53 -0300

    Parssing data into cells

--- AppStore/Service/Service.swift
+++ AppStore/Service/Service.swift
@@ -53,7 +53,6 @@ class Service {
             
             do {
                 let appGroup = try JSONDecoder().decode(AppGroup.self, from: data!)
-                appGroup.feed.results.forEach({print($0.name)})
                 completion(appGroup, nil)
             } catch let jsonError {
                 completion(nil, jsonError)
