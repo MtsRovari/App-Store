4bf1478a284832b3c9a499db5707b67cc5e7a38b
Mateus Rovari <mateusrovari@Mateuss-MacBook-Pro.local>
2019-11-16 22:36:25 -0300

    Handling user selection

---
+++ AppStore/Controller/Apps/Details/AppDetailController.swift
@@ -0,0 +1,17 @@
+//
+//  AppDetailController.swift
+//  AppStore
+//
+//  Created by Mateus Rovari on 16/11/19.
+//  Copyright © 2019 Mateus Rovari. All rights reserved.
+//
+
+import UIKit
+
+class AppDetailController: BaseListController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        collectionView.backgroundColor = .yellow
+    }
+}
